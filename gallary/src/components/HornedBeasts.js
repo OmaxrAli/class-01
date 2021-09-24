e970583a23f9d9261cbe95b49e97faf5ee70ffba
OmaxrAli <omar.abdelmajeed17@gmail.com>
2021-09-24 16:16:51 +0300

    lab 2

--- gallary/src/components/HornedBeasts.js
+++ gallary/src/components/HornedBeasts.js
@@ -1,20 +1,26 @@
 import React from "react";
-import Main from "./Main";
-
+import HB2 from "./HB2";
 class HornedBeasts extends React.Component {
 
-    render(){
 
+
+    
+    render(){
         return(
             <div>
-            <Main Title={'UniWhal'} />
+                
+                
+           <HB2 image_url= "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" title= "UniWhal"  description= "A unicorn and a narwhal nuzzling their horns" keyword = "narwhal"  horns = "1"
+        
+            />
 
-            <Main image_url={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}/>
-
-            <Main  description={'A unicorn and a narwhal nuzzling their horns'}/>
-            </div>
+            <HB2 image_url= "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" title= "Rhino Family"  description= "Mother (or father) rhino with two babies" keyword = "rhino"  horns = "2"
+             />
             
-        )
+                
+
+            </div>
+            )
     }
 }
 
